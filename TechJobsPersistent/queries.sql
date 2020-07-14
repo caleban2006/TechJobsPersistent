d5923c098d2efd3b0b3e2a04ae9bf8fc45352344
caleban2006 <logan.jen.2005@gmail.com>
2020-07-14 15:22:06 -0400

    updated SQL

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -3,4 +3,4 @@ DESCRIBE Jobs;
 --Part 2
 SELECT Name FROM employers WHERE Location = "St. Louis";
 --Part 3
-SELECT DISTINCT Name, Description FROM skills RIGHT JOIN jobskills ON skills.Id = jobskills.SkillId ORDER BY Name;
+SELECT DISTINCT Name, Description FROM skills JOIN jobskills ON skills.Id = jobskills.SkillId ORDER BY Name;
