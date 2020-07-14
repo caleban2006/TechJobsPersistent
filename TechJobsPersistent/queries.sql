5fd0244840aafb0b95d6f346c105c319c4aaef8f
caleban2006 <logan.jen.2005@gmail.com>
2020-07-14 14:45:10 -0400

    finished, I think

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,6 @@
 --Part 1
-SECRIBE Jobs;
+DESCRIBE Jobs;
 --Part 2
-
+SELECT Name FROM employers WHERE Location = "St. Louis";
 --Part 3
-
+SELECT DISTINCT Name, Description FROM skills RIGHT JOIN jobskills ON skills.Id = jobskills.SkillId ORDER BY Name;
